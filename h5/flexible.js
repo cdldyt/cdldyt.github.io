3db39125e8608ba3a422cf10d278221382a27d9c
lennon-xia <1136160757@qq.com>
2024-04-12 13:10:39 +0800

    add web-h5

---
+++ h5/flexible.js
@@ -0,0 +1,93 @@
+(function flexible(window, document) {
+  function resetFontSize() {
+    const size = (document.documentElement.clientWidth / 1920) * 37.5;
+    document.documentElement.style.fontSize = size + 'px';
+  }
+
+  // reset root font size on page show or resize
+  window.addEventListener('pageshow', resetFontSize);
+  window.addEventListener('resize', resetFontSize);
+})(window, document);
+var timeoutId;
+function showHiddenContent() {
+  var hiddenDiv = document.getElementById("group_5");
+  hiddenDiv.style.display = "block";
+  cancelHidden();
+}
+
+window.onload = function () {
+  // 获取 URL 中的参数
+  const params = new URLSearchParams(window.location.search);
+  const paramValue = params.get('sccrollToRoku');
+  if (paramValue === "true") {
+    scrollToRoku()
+  }
+  var qrcode = new QRCode("qrcode");
+
+  // 设置二维码内容为 URL 地址
+  var url = "https://example.com";
+  qrcode.makeCode(url);
+  swiper = new Swiper('#swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true, // Enable loop mode
+    uniqueNavElements: true,
+    autoplay: {
+      delay: 3000, // Autoplay delay in milliseconds
+      disableOnInteraction: false, // Enable autoplay even when user interacts with slider
+    },
+    centeredSlides: true,
+  });
+}
+function hiddenContent() {
+  timeoutId = setTimeout(function () {
+    var hiddenDiv = document.getElementById("group_5");
+    hiddenDiv.style.display = "none";
+  }
+    , 1000)
+}
+
+function cancelHidden() {
+  clearTimeout(timeoutId);
+}
+
+function scrollToRoku() {
+  var targetElement = document.getElementById('roku_group');
+  var targetOffsetTop = targetElement.offsetTop;
+  var head = document.getElementById('group_2')
+  window.scrollTo({
+    top: targetOffsetTop - head.offsetHeight,
+    behavior: 'smooth'
+  });
+}
+function hrefToRokuFaq() {
+  window.location.href = "./lanhu_rokutvfaq/roku_faq.html"
+}
+function scrollToTop() {
+  var targetElement = document.getElementById('group_1');
+  targetElement.scrollIntoView({ behavior: 'smooth' });
+}
+
+function showQRCode() {
+  var popup = document.getElementById('popup');
+  var listener = function (event) {
+    // 获取鼠标的当前位置
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+
+    // 设置弹窗的位置
+    popup.style.left = mouseX + 'px';
+    popup.style.top = mouseY + 'px';
+    console.log(event + "---------------------" + mouseY + "-------------" + mouseX)
+  };
+  document.onmousemove = listener
+  // 显示弹窗
+  popup.style.display = 'block';
+}
+function dismissQRCode() {
+  // 隐藏弹窗
+  var popup = document.getElementById('popup');
+  popup.style.display = 'none';
+  document.onmousemove = null;
+}
---
+++ h5/lanhu_rokutvfaq/ruku_faq.js
@@ -0,0 +1,73 @@
+(function flexible(window, document) {
+  function resetFontSize() {
+    const size = (document.documentElement.clientWidth / 1920) * 37.5;
+    document.documentElement.style.fontSize = size + 'px';
+  }
+
+  // reset root font size on page show or resize
+  window.addEventListener('pageshow', resetFontSize);
+  window.addEventListener('resize', resetFontSize);
+})(window, document);
+function redirectToHome() {
+  // 检查浏览器历史记录中是否有上一页
+  if (history.length > 1) {
+    // 如果有上一页，则直接跳转到上一页
+    history.back();
+  } else {
+    // 如果没有上一页，则跳转到首页
+    window.location.href = "../index.html";
+  }
+}
+
+function hrefToRoku() {
+  window.location.href = "../index.html?sccrollToRoku=true"
+}
+
+function changeOfIssueDivStatus(document, documentImg, documentSolution) {
+  // 获取元素的计算后样式
+  var computedStyle = window.getComputedStyle(documentSolution);
+
+  // 检查元素的display属性
+  var displayValue = computedStyle.getPropertyValue('display');
+  if (displayValue === 'none') {
+    document.style.color = 'rgba(33, 112, 253, 1)'
+    documentSolution.style.display = "block"
+    documentImg.src = "./img/Frame2.png"
+  } else {
+    document.style.color = 'rgba(51, 51, 51, 1)'
+    documentSolution.style.display = "none"
+    documentImg.src = "./img/Frame.png"
+  }
+}
+
+function question1() {
+  var text = document.getElementById('questionText1');
+  var img = document.getElementById('questionImg1')
+  var solution=document.getElementById('questionSolution1')
+  changeOfIssueDivStatus(text, img,solution)
+}
+
+function question2(){
+  var text = document.getElementById('questionText2');
+  var img = document.getElementById('questionImg2')
+  var solution=document.getElementById('questionSolution2')
+  changeOfIssueDivStatus(text, img,solution)
+}
+function question3(){
+  var text = document.getElementById('questionText3');
+  var img = document.getElementById('questionImg3')
+  var solution=document.getElementById('questionSolution3')
+  changeOfIssueDivStatus(text, img,solution)
+}
+function question4(){
+  var text = document.getElementById('questionText4');
+  var img = document.getElementById('questionImg4')
+  var solution=document.getElementById('questionSolution4')
+  changeOfIssueDivStatus(text, img,solution)
+}
+function question5(){
+  var text = document.getElementById('questionText5');
+  var img = document.getElementById('questionImg5')
+  var solution=document.getElementById('questionSolution5')
+  changeOfIssueDivStatus(text, img,solution)
+}
